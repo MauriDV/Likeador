ccbdd280f2e77755525c26c788590b3c5a2b41f0
Mauricio <mdellevedove12@gmail.com>
2016-02-04 20:46:26 -0300

    Update likeador.js

--- likeador.js
+++ likeador.js
@@ -1,7 +1,14 @@
 // ==UserScript==
-// @name Likear
-// @namespace Violentmonkey Scripts
-// @grant none
+// @name         Likeador
+// @version      0.1
+// @description  likear todo el contenido del Mi
+// @author       @Mauri934
+// @match        https://www.taringa.net/*
+// @match      http://www.taringa.net/*
+
+// @grant        none
 // ==/UserScript==
+/* jshint -W097 */
+//'use strict';
 
 $("#Feed-controls-mi ul").append("<li><a onclick=\"$('.require-login.button-action-s.action-vote.hastipsy').click()\">Likear</a></li>")
