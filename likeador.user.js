78300a409d0db5069d728b67b89cc0c217295af9
MauriDV <mdellevedove12@gmail.com>
2016-02-04 21:10:18 -0300

    script

---
+++ likeador.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name Likear
+// @namespace Violentmonkey Scripts
+// @grant none
+// ==/UserScript==
+
+$("#Feed-controls-mi ul").append("<li><a onclick=\"$('.require-login.button-action-s.action-vote.hastipsy').click()\">Likear</a></li>")
